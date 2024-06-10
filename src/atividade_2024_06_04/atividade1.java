015c4bb1edbda5db71d7b4a28a24a3279cdc71b6
rojinhas <ruthrojasdasilva12@gmail.com>
2024-06-10 11:45:50 -0300

    Atividades estrutura de decisão

---
+++ src/atividade_2024_06_04/atividade1.java
@@ -0,0 +1,28 @@
+package atividade_2024_06_04;
+
+import java.util.Locale;
+import java.util.Scanner;
+public class atividade1 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+
+		Scanner ler = new Scanner (System.in);
+		
+		int A = 2, B = 4, C = 5;
+		
+		if (A + B>C);
+		System.out.printf("A soma de A + B é maior que C");
+	}
+		{int A = 2, B = 2, C = 5;
+		
+		if (A+B<C); 
+		System.out.printf("A soma de A + B é menor que C");
+	}
+		{int A = 2, B = 2, C = 4;
+		
+		if (A+B==C); 
+		System.out.printf("A soma de A + B é maior que C"); 
+	}
+
+}
